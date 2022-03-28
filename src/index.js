465c417f2ad2cc51ab4b85b6b94b024bea293059
JoeSpin <jspinelli.html@gmail.com>
2022-03-28 14:32:19 -0400

    Revert "Merge branch 'main' of https://github.com/JoeSpin/Isle-Of-Madden"
    This reverts commit 0b4347f71466ff2efb5b18f15af603b846b73175, reversing
    changes made to 291c72c2dffaaf32d5b644116c3147026f9be0c5.

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './pages/home/Home';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import TeamPage from './pages/team/TeamPage';
+import TeamList from './pages/team/TeamList';
+import PlayerSearchPage from './pages/player/PlayerSearchPage'; 
+import './App.css'
+import RulesPage from './components/RulesPage';
+import About from './pages/about/About';
+import LeagueSchedulePage from './pages/LeagueSchedule/LeagueSchedulePage'
+import GameStatsPage from './pages/GameStatsPage';
+import PlayerPage from './pages/player/PlayerPage';
+import StandingsPage from './components/Standings/StandingsPage';
+import LeagueLeaderPage from './components/LeagueLeaders/LeagueLeaderPage';
+import Helmet from 'react-helmet';
+
+
+ReactDOM.render(
+  <>
+  <Helmet>
+    <title>Isle of Madden</title>
+    <meta
+      name="description"
+      content="The #1 PC Madden Franchise."
+    />
+    <meta property="og:image" content="https://www.isleofmadden.com/logo.png" />
+    <meta property="og:title" content="Isle of Madden"></meta>
+  </Helmet>
+  <Router>
+    <Route exact path="/" render={Home} />
+    <Route exact path="/team/:tn" render={TeamPage} />
+    <Route exact path="/team" render={TeamList} />
+    <Route exact path="/players" render={PlayerSearchPage} />
+    <Route exact path="/rules" render={RulesPage} />
+    <Route exact path="/about" render={About} /> 
+    <Route exact path="/schedule">
+      <LeagueSchedulePage />
+    </Route>
+    <Route exact path ="/player/:playerId">
+      <PlayerPage key={Date.now()}/>
+    </Route>
+    <Route exact path="/game/:gameId" >
+      <GameStatsPage key={Date.now()} />
+    </Route>
+    <Route exact path="/standings">
+      <StandingsPage />
+    </Route>
+    <Route exact path="/leagueleaders">
+      <LeagueLeaderPage />
+    </Route>
+  </Router>
+  </>,
+  document.getElementById('root')
+);
